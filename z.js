214aef48f38025da94ffea1aa453fd0451e32d0d
Mekha Krishnan M <mekhakrishnanm@gmail.com>
2020-06-09 00:59:42 +0800

    Code snippets

--- z.js
+++ z.js
@@ -9,7 +9,7 @@ res.multi().rpush(`${room}_messages`, message).execAsync()
 
 /**
   *Redis Publishing 'activeUsers'
- its called while fetching the active users in chatRoutes.js
+ It is called while fetching the active users in chatRoutes.js
  */
 client.publish('activeUsers', JSON.stringify(fetchUsers(room)))
 
@@ -34,10 +34,46 @@ res.smembersAsync(`${room}_users`)
  */
 res.multi().sadd(`${room}_users`, user).execAsync()
 /**
- *Redis removing a active user for a particular room 
+ *Redis removing an active user for a particular room 
  using SREM key Command.
  MULTI is used queued for atomic execution
  EXEC is executes all previously queued commands
  functions.js Line no: 98
  */
 res.multi().srem(`${room}_users`, user).execAsync()
+/** 
+ *Adding fields using Redisearch Modules when 
+ a product is added
+ adminRoutes.js : Lines 35-65
+*/
+client.createIndex([
+    client.fieldDefinition.numeric('itemId', true),
+    client.fieldDefinition.text('itemName', true),
+    client.fieldDefinition.text('itemType', true),
+    client.fieldDefinition.numeric('itemPrice', true),
+    client.fieldDefinition.text('itemDetails', true),
+    client.fieldDefinition.text('itemBrand', true),
+    client.fieldDefinition.text('itemColor', true),
+  ],function (error, val) {})
+client.add(itemId,{
+    itemId: itemId,
+    itemName: itemName,
+    itemType: itemType,
+    itemPrice: itemPrice,
+    itemDetails: itemDetails,
+    itemBrand: itemBrand,
+    itemColor: itemColor,
+  }, function (error, val) {})
+  /**
+   *Item Brands are added as a list to Redis to view the 
+   item brand list added to the platform
+    adminRoutes.js : Lines:66-67
+   */
+  await redisClient.lrem('itemBrand', 0, itemBrand.toUpperCase())
+  await redisClient.lpush('itemBrand', itemBrand.toUpperCase())
+  /**
+   *Initialise the item count of each product with 
+   respect to the item count as 0
+    adminRoutes.js : Line 68
+   */
+  await redisClient.zadd('itemCount', 0, itemId)
